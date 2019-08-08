f3a4d441279c7252b3cb7251982c9683cadb837f
nicolas <nicolas.ducteil@omnilive.fr>
2019-08-08 12:27:29 +0200

    ajout express csp middleware2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const cors = require('cors');
+const csp = require('csp');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
