24127863f6e276b22d5d1b261424bb5e543631ec
nicolas <nicolas.ducteil@omnilive.fr>
2019-08-26 15:42:11 +0200

    modif conditions register user

--- src/routes/users.js
+++ src/routes/users.js
@@ -29,15 +29,16 @@ export default function(app, passport, io) {
   userRouter.post('/register', (req, res) => {
 
     req.checkBody('username', 'Username field cannot be empty.').notEmpty();
-    req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
+    req.checkBody('username', 'Username must be between 5-20 characters long.').len(5, 20);
     req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
     req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
     req.checkBody('email', 'Email field cannot be empty.').notEmpty();
     req.checkBody('password', 'Password field cannot be empty.').notEmpty();
-    req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
-    req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
-    req.checkBody('confirmedPassword', 'Password must be between 8-100 characters long.').len(8, 100);
+    req.checkBody('password', 'Password must be between 8-50 characters long.').len(8, 50);
+    req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/);
+    req.checkBody('confirmedPassword', 'Password must be between 8-50 characters long.').len(8, 50);
     req.checkBody('confirmedPassword', 'Passwords do not match, please try again.').equals(req.body.password);
+    req.checkBody('role', 'Please choose a role').notEmpty();
 
     const errors = req.validationErrors();
 
